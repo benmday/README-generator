0da1c129e3c92d9b318ce6f141b153be8ded7e9b
benmday <benjaminmichaelday@gmail.com>
2022-11-09 12:09:36 -0800

    added prompts and printed to new README

---
+++ README.md
@@ -0,0 +1,13 @@
+{
+  "title": "peepee poopoo",
+  "description": "peepee poopoo",
+  "installation": "Clone this repo",
+  "usage": "",
+  "contributions": "",
+  "test": "",
+  "license": [
+    "WTFPL"
+  ],
+  "github": "benmday",
+  "email": "ben@ben.com"
+}
--- index.js
+++ index.js
@@ -1,13 +1,121 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [
+//   {
+//     type: "input",
+//     name: "title",
+//     message: "What is the title of your project?",
+//   },
+//   {
+//     type: "input",
+//     name: "description",
+//     message: "Write a brief description of your project:",
+//   },
+//   {
+//     type: "input",
+//     name: "installation",
+//     message: "Enter installation instructions:",
+//   },
+//   {
+//     type: "input",
+//     name: "usage",
+//     message: "Enter usage information:",
+//   },
+//   {
+//     type: "input",
+//     name: "contributions",
+//     message: "Enter contribution guidelines:",
+//   },
+//   {
+//     type: "input",
+//     name: "test",
+//     message: "Enter test instructions:",
+//   },
+//   {
+//     type: "checkbox",
+//     name: "license",
+//     message: "Choose a license:",
+//     choices: ["MIT", "Creative Commons", "WTFPL", "The Unlicense"],
+//   },
+//   {
+//     type: "input",
+//     name: "github",
+//     message: "Enter GitHub username:",
+//   },
+//   {
+//     type: "input",
+//     name: "email",
+//     message: "Enter your email:",
+//   },
+// ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//   fs.writeFile(fileName, data, (err) =>
+//     err ? console.error(err) : console.log("Template successfully created!")
+//   );
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project:",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Enter installation instructions:",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information:",
+      },
+      {
+        type: "input",
+        name: "contributions",
+        message: "Enter contribution guidelines:",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Enter test instructions:",
+      },
+      {
+        type: "checkbox",
+        name: "license",
+        message: "Choose a license:",
+        choices: ["MIT", "Creative Commons", "WTFPL", "The Unlicense"],
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter GitHub username:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email:",
+      },
+    ])
+    .then((response) =>
+      fs.writeFile("README.md", JSON.stringify(response, null, 2), (err) =>
+        err ? console.error(err) : console.log("Template successfully created!")
+      )
+    );
+}
 
 // Function call to initialize app
 init();
